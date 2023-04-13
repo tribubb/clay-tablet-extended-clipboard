c70d5e63372d3a3c205b938f73cd7c30728f734a
Triden Richards <tridenratu@hotmail.com>
2023-04-14 02:05:45 +1200

    testing revised code

--- primary.js
+++ primary.js
@@ -32,7 +32,7 @@ chrome.contextMenus.onClicked.addListener((info) => {
   }
 });
 
-// Remove the "Delete" context menu item when context menu is shown
+/*// Remove the "Delete" context menu item when context menu is shown
 chrome.contextMenus.onShown.addListener((info) => {
   // Remove the "Delete" context menu item
   chrome.contextMenus.remove('delete-context', () => {
@@ -48,4 +48,4 @@ chrome.contextMenus.onShown.addListener((info) => {
       }
     });
   });
-});
+});*/
