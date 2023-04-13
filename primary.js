54b1577948a459a7ea759efa2e2bb5fba97fa829
Triden Richards <tridenratu@hotmail.com>
2023-04-14 01:30:47 +1200

    adding editable permissions

--- primary.js
+++ primary.js
@@ -2,11 +2,13 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: 'show-main-context',
     title: 'Right-click Clipboard paste options',
+    contexts: ['editable']
   });
 
   chrome.contextMenus.create({
     id: 'show-second-context',
     title: 'Right-click Clipboard paste options 2',
+    contexts: ['editable']
   });
 });
 
