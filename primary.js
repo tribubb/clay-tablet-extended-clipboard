e07519d6650001b73352af04a00f6f1aa97353f4
Triden Richards <tridenratu@hotmail.com>
2023-04-14 01:54:40 +1200

    Adding delete function

--- primary.js
+++ primary.js
@@ -13,5 +13,27 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener((info) => {
-  console.log("Example action")
+  // Handle context menu click event here
+  if (info.menuItemId === 'show-main-context') 
+  {
+    console.log('Example action for show-main-context');
+  } 
+  else if (info.menuItemId === 'show-second-context')
+  {
+    console.log('Example action for show-second-context');
+  }
+});
+
+chrome.contextMenus.onShown.addListener((info) => {
+  // Create a new context menu item for "Delete" option
+  chrome.contextMenus.create({
+    id: 'delete-context',
+    title: 'Delete',
+    parentId: info.menuItemId, // Set parentId to the currently shown menu item
+    contexts: ['editable'],
+    onclick: (info, tab) => {
+      // Handle "Delete" option click event here
+      console.log('Delete clicked');
+    }
+  });
 });
