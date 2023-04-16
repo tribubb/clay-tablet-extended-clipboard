3fe7333750d9197e90f5214e40d54b7679c70954
Triden Richards <tridenratu@hotmail.com>
2023-04-17 10:54:33 +1200

    copy

--- contentScript.js
+++ contentScript.js
@@ -46,12 +46,23 @@ document.addEventListener("DOMContentLoaded", function () {
                 inputElement.type = "text";
                 inputElement.value = inputTextArray[i - 1];
 
-                var buttonElement = document.createElement("button");
-                buttonElement.textContent = "Remove";
+                // Create a copy button element
+                var copyElement = document.createElement("button");
+                copyElement.textContent = "Copy";
+                copyElement.className = "copyclass";
+
+                // Create a paste button element
+                var pasteElement = document.createElement("button");
+                pasteElement.textContent = "Paste";
+                pasteElement.className = "pasteclass";
+
+                var removeElement = document.createElement("button");
+                removeElement.textContent = "Remove";
+                removeElement.className = "removeclass";
 
                 // Use a closure to capture the correct value of i for each iteration
                 (function (index) {
-                    buttonElement.addEventListener("click", function () {
+                    removeElement.addEventListener("click", function () {
                         inputTextArray[index] = inputElement.value;
                         var containerElement = this.parentNode;
                         var listItemElement = containerElement.parentNode;
@@ -72,10 +83,48 @@ document.addEventListener("DOMContentLoaded", function () {
                         const bigString = randomStrings.join(randomStrings[index] + ',');
                         localStorage.setItem('myBigString', bigString);
                     });
+
+                    copyElement.addEventListener("click", function () {
+                        // Get the parent container element
+                        var containerElement = this.parentNode;
+
+                        // Get the input element within the parent container
+                        var inputElement = containerElement.querySelector("input[type='text']");
+
+                        // Get the value of the input element
+                        var inputValue = inputElement.value;
+
+                        // Copy the value to the clipboard
+                        navigator.clipboard.writeText(inputValue).then(function () {
+                            console.log("Value copied to clipboard: " + inputValue);
+                        }).catch(function (err) {
+                            console.error("Unable to copy value to clipboard", err);
+                        });
+                    });
+
+                    pasteElement.addEventListener("click", function () {
+                        // Get the parent container element
+                        var containerElement = this.parentNode;
+
+                        // Get the input element within the parent container
+                        var inputElement = containerElement.querySelector("input[type='text']");
+
+                        // Read the text from the clipboard
+                        navigator.clipboard.readText().then(function (text) {
+                            // Set the value of the input element with the retrieved text
+                            inputElement.value = text;
+                            inputTextArray[index] = text; // Update the inputTextArray with the pasted text
+                            localStorage.setItem("inputTextData", JSON.stringify(inputTextArray)); // Update localStorage with the updated inputTextArray
+                        }).catch(function (err) {
+                            console.error("Unable to read text from clipboard", err);
+                        });
+                    });
                 })(i - 1); // Pass in the value of i - 1 to the closure
 
                 containerElement.appendChild(inputElement);
-                containerElement.appendChild(buttonElement);
+                containerElement.appendChild(copyElement);
+                containerElement.appendChild(pasteElement);
+                containerElement.appendChild(removeElement);
 
                 listItemElement.appendChild(containerElement);
 
@@ -101,12 +150,26 @@ document.addEventListener("DOMContentLoaded", function () {
         inputElement.type = "text";
         inputElement.value = "";
 
-        // Create a button element
-        var buttonElement = document.createElement("button");
-        buttonElement.textContent = "Remove";
+        // Create a copy button element
+        var copyElement = document.createElement("button");
+        copyElement.textContent = "Copy";
+        copyElement.className = "copyclass";
+
+        // Create a paste button element
+        var pasteElement = document.createElement("button");
+        pasteElement.textContent = "Paste";
+        pasteElement.className = "pasteclass";
+
+        // Create a remove button element
+        var removeElement = document.createElement("button");
+        removeElement.textContent = "Remove";
+        removeElement.className = "removeclass";
+
+        // Get the index of the input element
+        var index = inputTextArray.length;
 
         // Add click event listener to the remove button
-        buttonElement.addEventListener("click", function () {
+        removeElement.addEventListener("click", function () {
             // Get the parent container element
             var containerElement = this.parentNode;
 
@@ -129,21 +192,60 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
 
+        // Add click event listener to the copy button
+        copyElement.addEventListener("click", function () {
+            // Get the input element value
+            var inputValue = inputElement.value;
+
+            // Copy the input value to clipboard
+            navigator.clipboard.writeText(inputValue)
+        });
+
+        // Add click event listener to the paste button
+        pasteElement.addEventListener("click", function () {
+            // Get the parent container element
+            var containerElement = this.parentNode;
+
+            // Get the input element within the parent container
+            var inputElement = containerElement.querySelector("input[type='text']");
+
+            // Read the text from the clipboard
+            navigator.clipboard.readText().then(function (text) {
+                // Set the value of the input element with the retrieved text
+                inputElement.value = text;
+                inputTextArray[index] = text; // Update the inputTextArray with the pasted text
+                localStorage.setItem("inputTextData", JSON.stringify(inputTextArray)); // Update localStorage with the updated inputTextArray
+
+                // Update the listScore element with the retrieved text
+                document.getElementById("listScore").textContent = text;
+            }).catch(function (err) {
+                console.error("Unable to read text from clipboard", err);
+            });
+        });
+
         // Add input event listener to the input element
         inputElement.addEventListener("input", function () {
-            // Get the index of the input element
-            var index = Array.from(this.parentNode.parentNode.children).indexOf(this.parentNode);
-
             // Update the corresponding element in the inputTextArray
             inputTextArray[index] = this.value;
 
             // Save the updated input text data to localStorage
             localStorage.setItem("inputTextData", JSON.stringify(inputTextArray)); // Convert array to JSON string and save to localStorage
+
+            // Update the corresponding element in the randomStrings array
+            randomStrings[index] = this.value;
+            const bigString = randomStrings.join(",");
+            localStorage.setItem("myBigString", bigString);
         });
+
+        // Push the new input value into the inputTextArray and randomStrings array
+        inputTextArray.push("");
+        randomStrings.push("");
 
         // Append the input element and button element to the container element
         containerElement.appendChild(inputElement);
-        containerElement.appendChild(buttonElement);
+        containerElement.appendChild(copyElement);
+        containerElement.appendChild(pasteElement);
+        containerElement.appendChild(removeElement);
 
         // Append the container element to the list item element
         listItemElement.appendChild(containerElement);
@@ -153,5 +255,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Save the updated list data to localStorage
         localStorage.setItem("listData", listElement.innerHTML);
-        });
     });
+});
